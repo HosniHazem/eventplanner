2c4ff6ebba85b13400c4223fd6b1f116f0c8c8a6
Hazem Hosni <hazem.hosni@aiesec.net>
2024-04-14 13:08:43 +0100

    bugs fix onbuilding phase

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -31,8 +31,10 @@ const populateEvent = (query: any) => {
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase()
-
     const organizer = await User.findById(userId)
+    console.log(organizer)
+    console.log(userId)
+    
     if (!organizer) throw new Error('Organizer not found')
 
     const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
